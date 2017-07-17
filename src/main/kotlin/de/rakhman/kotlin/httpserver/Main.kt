9bf87bf652f4ed38a1267aa1188ac046b3fce1c6
Kirill Rakhman <kirill.rakhman@gmail.com>
2017-07-17 22:27:14 +0200

    implement for demo

--- src/main/kotlin/de/rakhman/kotlin/httpserver/Main.kt
+++ src/main/kotlin/de/rakhman/kotlin/httpserver/Main.kt
@@ -1,4 +1,34 @@
 package de.rakhman.kotlin.httpserver
 
+import com.google.gson.JsonObject
+import fi.iki.elonen.NanoHTTPD.Method.GET
+
 fun main(args: Array<String>) {
+    val server = Server()
+
+    server.register(GET, "/hello") {
+        val json = JsonObject()
+        val name = this.session.parms["name"]
+        json.addProperty("text", "Hello, $name")
+        json
+    }
+
+    server.register<Person>("/register") { person ->
+        responseHeaders.put("X-Foo", "Bar")
+
+        RegistrationResult(person, registered = true)
+    }
+
+    server.start()
+    readLine()
 }
+
+class Person(
+        val name: String,
+        val age: Int
+)
+
+class RegistrationResult(
+        val person: Person,
+        val registered: Boolean
+)
--- src/main/kotlin/de/rakhman/kotlin/httpserver/Server.kt
+++ src/main/kotlin/de/rakhman/kotlin/httpserver/Server.kt
@@ -1,9 +1,58 @@
 package de.rakhman.kotlin.httpserver
 
+import com.google.gson.Gson
+import com.google.gson.JsonElement
 import fi.iki.elonen.NanoHTTPD
+import fi.iki.elonen.NanoHTTPD.Method.POST
+import fi.iki.elonen.NanoHTTPD.Response.Status.NOT_FOUND
+import fi.iki.elonen.NanoHTTPD.Response.Status.OK
 
-abstract class AbstractServer: NanoHTTPD(8080) {
+class Server : NanoHTTPD(8080) {
+
+    val gson = Gson()
+    val routes: MutableMap<Pair<Method, String>, Context.() -> Any> = mutableMapOf()
+
+    fun register(method: Method, path: String, route: Context.() -> Any) {
+        routes.put(method to path) {
+            route()
+        }
+    }
+
+    inline fun <reified T> register(path: String, crossinline route: Context.(T) -> Any) {
+        routes.put(POST to path) {
+            val body = gson.fromJson<T>(session.getBody(), T::class.java)
+            route(body)
+        }
+
+    }
+
+    private fun notFound() = newFixedLengthResponse(NOT_FOUND, "text/plain", "Not found")
+
     override fun serve(session: IHTTPSession): Response {
-        return newFixedLengthResponse(Response.Status.NOT_FOUND,"application/json", "Not found")
+        val method = session.method
+        val path = session.uri
+
+        val key = method to path
+        val route = routes[key] ?: return notFound()
+
+        val context = Context(session)
+        val result = context.route()
+
+        val response = when (result) {
+            is Response -> result
+            is JsonElement -> newFixedLengthResponse(OK, "application/json", result.toString())
+            else -> newFixedLengthResponse(OK, "application/json", gson.toJson(result))
+        }
+
+        for ((name, value) in context.responseHeaders) {
+            response.addHeader(name, value)
+        }
+
+        return response
     }
 }
+
+class Context(
+        val session: NanoHTTPD.IHTTPSession,
+        val responseHeaders: MutableMap<String, String> = mutableMapOf()
+)
